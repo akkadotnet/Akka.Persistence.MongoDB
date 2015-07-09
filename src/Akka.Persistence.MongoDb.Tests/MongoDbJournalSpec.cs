db785914cc73b3fbf9dad9ef19a5e06cc6de8ad2
Silv3rcircl3 <piechura@gmx.net>
2015-07-09 10:02:59 +0200

    Hopefully fixed #4 Instead of creating a MongoDbRunner per test, we now use only one for all journal and another one for alle snapshot tests.

--- src/Akka.Persistence.MongoDb.Tests/MongoDbJournalSpec.cs
+++ src/Akka.Persistence.MongoDb.Tests/MongoDbJournalSpec.cs
@@ -1,11 +1,15 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using Akka.Persistence.TestKit.Journal;
 using Mongo2Go;
+using MongoDB.Driver;
 
 namespace Akka.Persistence.MongoDb.Tests
 {
     public class MongoDbJournalSpec : JournalSpec
     {
+        private static readonly MongoDbRunner Runner = MongoDbRunner.Start(ConfigurationManager.AppSettings[0]);
+
         private static readonly string SpecConfig = @"
         akka.persistence {
             publish-plugin-commands = on
@@ -19,29 +23,25 @@ namespace Akka.Persistence.MongoDb.Tests
             }
         }";
 
-        private static MongoDbRunner _runner;
-
         public MongoDbJournalSpec() : base(CreateSpecConfig(), "MongoDbJournalSpec")
         {
+            AppDomain.CurrentDomain.DomainUnload += (_, __) => Runner.Dispose();
+
             Initialize();
         }
 
         private static string CreateSpecConfig()
         {
-            _runner = MongoDbRunner.Start(ConfigurationManager.AppSettings[0]);
-            return SpecConfig.Replace("<ConnectionString>", _runner.ConnectionString + "akkanet");
+            return SpecConfig.Replace("<ConnectionString>", Runner.ConnectionString + "akkanet");
         }
 
         protected override void Dispose(bool disposing)
         {
+            new MongoClient(Runner.ConnectionString)
+                .GetDatabase("akkanet")
+                .DropCollectionAsync("EventJournal").Wait();
+            
             base.Dispose(disposing);
-
-            try
-            {
-                _runner.Dispose();
-            }
-            catch { }
-            _runner = null;
         }
     }
 }
