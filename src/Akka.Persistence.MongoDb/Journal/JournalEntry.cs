b7e6f551a325c3962349c73b3d7cd895dd21838f
Andrea Balducci <andrea.balducci@prxm.local>
2016-04-12 23:41:34 +0200

    Updated persistence (incomplete)

--- src/Akka.Persistence.MongoDb/Journal/JournalEntry.cs
+++ src/Akka.Persistence.MongoDb/Journal/JournalEntry.cs
@@ -1,10 +1,11 @@
-﻿//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // <copyright file="JournalEntry.cs" company="Akka.NET Project">
 //     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
 //     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
 // </copyright>
 //-----------------------------------------------------------------------
 
+﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Akka.Persistence.MongoDb.Journal
@@ -32,5 +33,16 @@ namespace Akka.Persistence.MongoDb.Journal
 
         [BsonElement("Manifest")]
         public string Manifest { get; set; }
+    }
+
+
+    public class JournalCommit
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Entries")]
+        public JournalEntry[] Entries { get; set; }
+
     }
 }
