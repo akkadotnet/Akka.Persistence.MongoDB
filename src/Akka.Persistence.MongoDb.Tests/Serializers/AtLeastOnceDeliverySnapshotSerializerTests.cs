ccb67734dd0badf51e398934ea4d6073ce9dcb93
Maxim <maxx1337@gmail.com>
2016-05-25 12:07:39 +0200

    add support for AtLeastOnceDeliverySnapshot

---
+++ src/Akka.Persistence.MongoDb.Tests/Serializers/AtLeastOnceDeliverySnapshotSerializerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MongoDB.Bson;
+using Akka.Persistence.MongoDb.Snapshot.Serializers;
+using MongoDB.Bson.Serialization;
+
+namespace Akka.Persistence.MongoDb.Tests.Serializers
+{
+    public class AtLeastOnceDeliverySnapshotSerializerTests
+    {
+        class TestMessage
+        {
+            public TestMessage(string text)
+            {
+                this.Text = text;
+            }
+            public string Text { get; private set; }
+        }
+
+        static AtLeastOnceDeliverySnapshotSerializerTests()
+        {
+            BsonSerializer.RegisterSerializer<AtLeastOnceDeliverySnapshot>(new AtLeastOnceDeliverySnapshotSerializer());
+            BsonSerializer.RegisterSerializer<UnconfirmedDelivery>(new UnconfirmedDeliverySerializer());
+        }
+
+        [Fact]
+        public void TestEmptySnapshot()
+        {
+            var snapshot = new AtLeastOnceDeliverySnapshot(1, new UnconfirmedDelivery[0]);
+
+            var json = snapshot.ToJson();
+            var expected = "{ '_t' : 'AtLeastOnceDeliverySnapshot', 'CurrentDeliveryId' : NumberLong(1), 'UnconfirmedDeliveries' : { '_t' : 'UnconfirmedDelivery[]', '_v' : [] } }"
+                .Replace("'", "\"");
+            Assert.Equal(expected, json);
+
+            var bson = snapshot.ToBson();
+            var rehydrated = BsonSerializer.Deserialize<AtLeastOnceDeliverySnapshot>(bson);
+            Assert.True(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Fact]
+        public void TestSnapshotWithUnconfirmedDelivery()
+        {
+            var snapshot = new AtLeastOnceDeliverySnapshot(1, new[] { new UnconfirmedDelivery(1, Actor.ActorPath.Parse("akka://MySystem/user/test"), new TestMessage("Test message")) });
+
+            var json = snapshot.ToJson();
+            var expected = "{ '_t' : 'AtLeastOnceDeliverySnapshot', 'CurrentDeliveryId' : NumberLong(1), 'UnconfirmedDeliveries' : { '_t' : 'UnconfirmedDelivery[]', '_v' : [{ '_t' : 'UnconfirmedDelivery', 'DeliveryId' : NumberLong(1), 'Destination' : 'akka://MySystem/user/test', 'Message' : { '_t' : 'TestMessage', 'Text' : 'Test message' } }] } }"
+                .Replace("'", "\"");
+            Assert.Equal(expected, json);
+
+            var bson = snapshot.ToBson();
+            var rehydrated = BsonSerializer.Deserialize<AtLeastOnceDeliverySnapshot>(bson);
+            Assert.True(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+    }
+}
---
+++ src/Akka.Persistence.MongoDb/Snapshot/Serializers/AtLeastOnceDeliverySnapshotSerializer.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace Akka.Persistence.MongoDb.Snapshot.Serializers
+{
+    public class AtLeastOnceDeliverySnapshotSerializer : SerializerBase<AtLeastOnceDeliverySnapshot>
+    {
+        public override Akka.Persistence.AtLeastOnceDeliverySnapshot Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var currentBsonType = context.Reader.GetCurrentBsonType();
+
+            if (currentBsonType == MongoDB.Bson.BsonType.Null)
+            {
+                context.Reader.ReadNull();
+                return null;
+            }
+
+            context.Reader.ReadStartDocument();
+
+            string object_type = context.Reader.ReadString();
+
+            long currentDeliveryId = context.Reader.ReadInt64();
+            UnconfirmedDelivery[] unconfirmedDeliveries = null;
+
+            var name = context.Reader.ReadName(MongoDB.Bson.IO.Utf8NameDecoder.Instance);
+            if (name == "UnconfirmedDeliveries")
+            {
+                unconfirmedDeliveries = BsonSerializer.Deserialize<UnconfirmedDelivery[]>(context.Reader);
+            }
+
+            context.Reader.ReadEndDocument();
+
+            var result = new AtLeastOnceDeliverySnapshot(currentDeliveryId, unconfirmedDeliveries);
+            return result;
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, AtLeastOnceDeliverySnapshot value)
+        {
+            context.Writer.WriteStartDocument();
+
+            context.Writer.WriteName("_t");
+            context.Writer.WriteString(value.GetType().Name);
+
+            context.Writer.WriteName("CurrentDeliveryId");
+            context.Writer.WriteInt64(value.CurrentDeliveryId);
+
+            context.Writer.WriteName("UnconfirmedDeliveries");
+            BsonSerializer.Serialize<UnconfirmedDelivery[]>(context.Writer, value.UnconfirmedDeliveries);
+
+            context.Writer.WriteEndDocument();
+        }
+    }
+}
