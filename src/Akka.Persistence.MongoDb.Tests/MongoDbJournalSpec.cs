9f6ae6f98e086dc435b1ec0b67cba576e2744756
Aleksandr Evdokimov <larsus.develop@gmail.com>
2018-04-07 21:04:17 +0500

    Add binary format tests

--- src/Akka.Persistence.MongoDb.Tests/MongoDbJournalSpec.cs
+++ src/Akka.Persistence.MongoDb.Tests/MongoDbJournalSpec.cs
@@ -41,4 +41,36 @@ namespace Akka.Persistence.MongoDb.Tests
             return ConfigurationFactory.ParseString(specString);
         }
     }
+
+    [Collection("MongoDbSpec")]
+    public class MongoDbBinaryJournalSpec : JournalSpec, IClassFixture<DatabaseFixture>
+    {
+        protected override bool SupportsRejectingNonSerializableObjects { get; } = false;
+
+        public MongoDbBinaryJournalSpec(DatabaseFixture databaseFixture) : base(CreateSpecConfig(databaseFixture), "MongoDbJournalSpec")
+        {
+            Initialize();
+        }
+
+        private static Config CreateSpecConfig(DatabaseFixture databaseFixture)
+        {
+            var specString = @"
+                akka.test.single-expect-default = 3s
+                akka.persistence {
+                    publish-plugin-commands = on
+                    journal {
+                        plugin = ""akka.persistence.journal.mongodb""
+                        mongodb {
+                            class = ""Akka.Persistence.MongoDb.Journal.MongoDbJournal, Akka.Persistence.MongoDb""
+                            connection-string = """ + databaseFixture.ConnectionString + @"""
+                            auto-initialize = on
+                            collection = ""EventJournal""
+                            stored-as = binary
+                        }
+                    }
+                }";
+
+            return ConfigurationFactory.ParseString(specString);
+        }
+    }
 }
