0d3ab9759760d17ed644ce12025a4ce9ad91cbee
Aaron Stannard <aaron@petabridge.com>
2019-09-24 08:37:09 -0500

    added all Akka.Persistence.TCK.Serialization specs

---
+++ src/Akka.Persistence.MongoDb.Tests/Serialization/MongoDbSnapshotStoreBinarySerializationSpec.cs
@@ -0,0 +1,45 @@
+﻿using Akka.Configuration;
+using Akka.Persistence.TCK.Serialization;
+using Akka.Util.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.MongoDb.Tests.Serialization
+{
+    [Collection("MongoDbSpec")]
+    public class MongoDbSnapshotStoreBinarySerializationSpec : SnapshotStoreSerializationSpec, IClassFixture<DatabaseFixture>
+    {
+        public static readonly AtomicCounter Counter = new AtomicCounter(0);
+        private readonly ITestOutputHelper _output;
+
+        public MongoDbSnapshotStoreBinarySerializationSpec(ITestOutputHelper output, DatabaseFixture databaseFixture)
+            : base(CreateSpecConfig(databaseFixture, Counter.GetAndIncrement()), nameof(MongoDbSnapshotStoreBinarySerializationSpec), output)
+        {
+            _output = output;
+            output.WriteLine(databaseFixture.ConnectionString + Counter.Current);
+        }
+
+        private static Config CreateSpecConfig(DatabaseFixture databaseFixture, int id)
+        {
+            var specString = @"
+                akka.persistence {
+                    publish-plugin-commands = on
+                    snapshot-store {
+                        plugin = ""akka.persistence.snapshot-store.mongodb""
+                        mongodb {
+                            class = ""Akka.Persistence.MongoDb.Snapshot.MongoDbSnapshotStore, Akka.Persistence.MongoDb""
+                            connection-string = """ + databaseFixture.ConnectionString + id + @"""
+                            auto-initialize = on
+                            collection = ""SnapshotStore""
+                            stored-as = binary
+                        }
+                    }
+                }";
+
+            return ConfigurationFactory.ParseString(specString);
+        }
+    }
+}
---
+++ src/Akka.Persistence.MongoDb.Tests/Serialization/MongoDbSnapshotStoreObjectSerializationSpec.cs
@@ -0,0 +1,42 @@
+﻿using Akka.Configuration;
+using Akka.Persistence.TCK.Serialization;
+using Akka.Util.Internal;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.MongoDb.Tests.Serialization
+{
+    [Collection("MongoDbSpec")]
+    public class MongoDbSnapshotStoreObjectSerializationSpec : SnapshotStoreSerializationSpec, IClassFixture<DatabaseFixture>
+    {
+        public static readonly AtomicCounter Counter = new AtomicCounter(0);
+        private readonly ITestOutputHelper _output;
+
+        public MongoDbSnapshotStoreObjectSerializationSpec(ITestOutputHelper output, DatabaseFixture databaseFixture)
+            : base(CreateSpecConfig(databaseFixture, Counter.GetAndIncrement()), nameof(MongoDbSnapshotStoreObjectSerializationSpec), output)
+        {
+            _output = output;
+            output.WriteLine(databaseFixture.ConnectionString + Counter.Current);
+        }
+
+        private static Config CreateSpecConfig(DatabaseFixture databaseFixture, int id)
+        {
+            var specString = @"
+                akka.test.single-expect-default = 3s
+                akka.persistence {
+                    publish-plugin-commands = on
+                    snapshot-store {
+                        plugin = ""akka.persistence.snapshot-store.mongodb""
+                        mongodb {
+                            class = ""Akka.Persistence.MongoDb.Snapshot.MongoDbSnapshotStore, Akka.Persistence.MongoDb""
+                            connection-string = """ + databaseFixture.ConnectionString + id + @"""
+                            auto-initialize = on
+                            collection = ""SnapshotStore""
+                        }
+                    }
+                }";
+
+            return ConfigurationFactory.ParseString(specString);
+        }
+    }
+}
