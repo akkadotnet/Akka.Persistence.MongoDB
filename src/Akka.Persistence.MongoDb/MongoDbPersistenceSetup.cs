9f458dba53582d90b82225a2fe770ba5e7b36cde
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-04-24 01:00:16 +0700

    Add Setup class

---
+++ src/Akka.Persistence.MongoDb/MongoDbPersistenceSetup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor.Setup;
+using MongoDB.Driver;
+
+namespace Akka.Persistence.MongoDb
+{
+    public class MongoDbPersistenceSetup : Setup
+    {
+        public MongoDbPersistenceSetup(
+            string snapshotDatabaseName,
+            MongoClientSettings snapshotConnectionSettings, 
+            string journalDatabaseName,
+            MongoClientSettings journalConnectionSettings)
+        {
+            SnapshotConnectionSettings = snapshotConnectionSettings;
+            JournalConnectionSettings = journalConnectionSettings;
+            SnapshotDatabaseName = snapshotDatabaseName;
+            JournalDatabaseName = journalDatabaseName;
+        }
+
+        public string SnapshotDatabaseName { get; }
+        public MongoClientSettings SnapshotConnectionSettings { get; }
+        public string JournalDatabaseName { get; }
+        public MongoClientSettings JournalConnectionSettings { get; }
+    }
+}
