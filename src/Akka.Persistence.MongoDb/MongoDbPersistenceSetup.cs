319e264ce9334101a3cc61192493f4537130f986
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-04-24 03:20:24 +0700

    Add xml-doc to setup class

--- src/Akka.Persistence.MongoDb/MongoDbPersistenceSetup.cs
+++ src/Akka.Persistence.MongoDb/MongoDbPersistenceSetup.cs
@@ -2,12 +2,32 @@
 using System.Collections.Generic;
 using System.Text;
 using Akka.Actor.Setup;
+using Akka.Persistence.MongoDb.Journal;
+using Akka.Persistence.MongoDb.Snapshot;
 using MongoDB.Driver;
 
 namespace Akka.Persistence.MongoDb
 {
+    /// <summary>
+    /// A setup class to allow for programmatic configuration of the <see cref="MongoDbSnapshotStore"/> and <see cref="MongoDbJournal"/>
+    /// </summary>
     public class MongoDbPersistenceSetup : Setup
     {
+        /// <summary>
+        /// Constructor to instantiate a new instance of <see cref="MongoDbPersistenceSetup"/>
+        /// </summary>
+        /// <param name="snapshotDatabaseName">
+        /// The database name to be used by <see cref="MongoDbSnapshotStore"/> when it connects the server
+        /// </param>
+        /// <param name="snapshotConnectionSettings">
+        /// The <see cref="MongoClientSettings"/> to be used by <see cref="MongoDbSnapshotStore"/> when it connects the server.
+        /// </param>
+        /// <param name="journalDatabaseName">
+        /// The database name to be used by <see cref="MongoDbJournal"/> when it connects the server
+        /// </param>
+        /// <param name="journalConnectionSettings">
+        /// The <see cref="MongoClientSettings"/> to be used by <see cref="MongoDbJournal"/> when it connects the server
+        /// </param>
         public MongoDbPersistenceSetup(
             string snapshotDatabaseName,
             MongoClientSettings snapshotConnectionSettings, 
@@ -20,9 +40,25 @@ namespace Akka.Persistence.MongoDb
             JournalDatabaseName = journalDatabaseName;
         }
 
+        /// <summary>
+        /// The database name to be used by <see cref="MongoDbSnapshotStore"/> when it connects the server
+        /// </summary>
         public string SnapshotDatabaseName { get; }
+        /// <summary>
+        /// The <see cref="MongoClientSettings"/> to be used by <see cref="MongoDbSnapshotStore"/> when it connects the server.
+        /// Setting this property will override 'akka.persistence.snapshot-store.mongodb.connection-string' in the HOCON configuration.
+        /// Set this to <code>null</code> if you're not overriding this connection string.
+        /// </summary>
         public MongoClientSettings SnapshotConnectionSettings { get; }
+        /// <summary>
+        /// The database name to be used by <see cref="MongoDbJournal"/> when it connects the server
+        /// </summary>
         public string JournalDatabaseName { get; }
+        /// <summary>
+        /// The <see cref="MongoClientSettings"/> to be used by <see cref="MongoDbJournal"/> when it connects the server
+        /// Setting this property will override 'akka.persistence.journal.mongodb.connection-string' in the HOCON configuration.
+        /// Set this to <code>null</code> if you're not overriding this connection string.
+        /// </summary>
         public MongoClientSettings JournalConnectionSettings { get; }
     }
 }
