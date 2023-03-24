395fc1754886aa85d8a79658d835d679f2018bbc
Edgars Roze <edgars.roze@gmail.com>
2023-03-24 14:15:08 +0200

    fixed ObjectSerializer initialization for backward compatibility (#310)

--- src/Akka.Persistence.MongoDb/FullTypeNameObjectSerializer.cs
+++ src/Akka.Persistence.MongoDb/FullTypeNameObjectSerializer.cs
@@ -24,7 +24,7 @@ namespace Akka.Persistence.MongoDb
         /// <summary>
         /// Initializes a new instance of the <see cref="FullTypeNameObjectSerializer"/> class.
         /// </summary>
-        public FullTypeNameObjectSerializer() : base(FullTypeNameDiscriminatorConvention.Instance) { }
+        public FullTypeNameObjectSerializer() : base(FullTypeNameDiscriminatorConvention.Instance, AllAllowedTypes) { }
 
         /// <summary>
         /// Deserializes a value.
