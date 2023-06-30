a45b91be76ded26895f8f230ddfb2e93a9111adb
Michael Buck <mhbuck@gmail.com>
2023-06-30 18:45:00 +0200

    Bit more clean up

--- src/Akka.Persistence.MongoDb.Hosting.Tests/MongoDbJournalOptionsSpec.cs
+++ src/Akka.Persistence.MongoDb.Hosting.Tests/MongoDbJournalOptionsSpec.cs
@@ -1,4 +1,7 @@
-﻿using Akka.Configuration;
+﻿using System;
+using Akka.Configuration;
+using Akka.Persistence.Journal;
+using Akka.Util;
 using FluentAssertions;
 using Xunit;
 
@@ -32,7 +35,15 @@ namespace Akka.Persistence.MongoDb.Hosting.Tests
             config.Should().NotBeNull();
             baseConfig.Should().NotBeNull();
 
-            AssertJournalConfig(config, baseConfig);
+            config.GetString("class").Should().Be(baseConfig.GetString("class"));
+            config.GetString("connection-string").Should().Be(baseConfig.GetString("connection-string"));
+            config.GetBoolean("use-write-transaction").Should().Be(baseConfig.GetBoolean("use-write-transaction"));
+            config.GetBoolean("auto-initialize").Should().Be(baseConfig.GetBoolean("auto-initialize"));
+            config.GetString("plugin-dispatcher").Should().Be(baseConfig.GetString("plugin-dispatcher"));
+            config.GetString("collection").Should().Be(baseConfig.GetString("collection"));
+            config.GetString("metadata-collection").Should().Be(baseConfig.GetString("metadata-collection"));
+            config.GetBoolean("legacy-serialization").Should().Be(baseConfig.GetBoolean("legacy-serialization"));
+            config.GetTimeSpan("call-timeout").Should().Be(baseConfig.GetTimeSpan("call-timeout"));
         }
 
         [Fact(DisplayName = "Empty MongoDbJournalOptions with custom identifier should equal empty config with default fallback")]
@@ -50,118 +61,98 @@ namespace Akka.Persistence.MongoDb.Hosting.Tests
             config.Should().NotBeNull();
             baseConfig.Should().NotBeNull();
 
-            AssertJournalConfig(config, baseConfig);
+            config.GetString("class").Should().Be(baseConfig.GetString("class"));
+            config.GetString("connection-string").Should().Be(baseConfig.GetString("connection-string"));
+            config.GetBoolean("use-write-transaction").Should().Be(baseConfig.GetBoolean("use-write-transaction"));
+            config.GetBoolean("auto-initialize").Should().Be(baseConfig.GetBoolean("auto-initialize"));
+            config.GetString("plugin-dispatcher").Should().Be(baseConfig.GetString("plugin-dispatcher"));
+            config.GetString("collection").Should().Be(baseConfig.GetString("collection"));
+            config.GetString("metadata-collection").Should().Be(baseConfig.GetString("metadata-collection"));
+            config.GetBoolean("legacy-serialization").Should().Be(baseConfig.GetBoolean("legacy-serialization"));
+            config.GetTimeSpan("call-timeout").Should().Be(baseConfig.GetTimeSpan("call-timeout"));
         }
 
-//        [Fact(DisplayName = "MongoDbJournalOptions should generate proper config")]
-//        public void JournalOptionsTest()
-//        {
-//            var options = new MongoDbJournalOptions(true)
-//            {
-//                Identifier = "custom",
-//                AutoInitialize = true,
-//                ConnectionString = "testConnection",
-//                Collection = "testCollection",
-//                MetadataCollection = "metadataCollection",
-//                UseWriteTransaction = true,
-//                LegacySerialization = true,
-//                CallTimeout = TimeSpan.FromHours(2)
-//            };
-//            options.Adapters.AddWriteEventAdapter<EventAdapters.EventMapper1>("mapper1", new[] { typeof(EventAdapters.Event1) });
-//            options.Adapters.AddReadEventAdapter<EventAdapters.ReadAdapter>("reader1", new[] { typeof(EventAdapters.Event1) });
-//            options.Adapters.AddEventAdapter<EventAdapters.ComboAdapter>("combo", boundTypes: new[] { typeof(EventAdapters.Event2) });
-//            options.Adapters.AddWriteEventAdapter<EventAdapters.Tagger>("tagger", boundTypes: new[] { typeof(EventAdapters.Event1), typeof(EventAdapters.Event2) });
-
-//            var baseConfig = options.ToConfig();
-
-//            baseConfig.GetString("akka.persistence.journal.plugin").Should().Be("akka.persistence.journal.custom");
-
-//            var config = baseConfig.GetConfig("akka.persistence.journal.custom");
-//            config.Should().NotBeNull();
-//            config.GetString("connection-string").Should().Be(options.ConnectionString);
-//            config.GetTimeSpan("connection-timeout").Should().Be(options.ConnectionTimeout);
-//            config.GetString("schema-name").Should().Be(options.SchemaName);
-//            config.GetString("table-name").Should().Be(options.TableName);
-//            config.GetBoolean("auto-initialize").Should().Be(options.AutoInitialize);
-//            config.GetString("metadata-table-name").Should().Be(options.MetadataTableName);
-//            config.GetBoolean("sequential-access").Should().Be(options.SequentialAccess);
-//            config.GetString("stored-as").Should().Be(options.StoredAs.ToHocon());
-//            config.GetBoolean("use-bigint-identity-for-ordering-column").Should().Be(options.UseBigIntIdentityForOrderingColumn);
-
-//            config.GetStringList($"event-adapter-bindings.\"{typeof(EventAdapters.Event1).TypeQualifiedName()}\"").Should()
-//                .BeEquivalentTo("mapper1", "reader1", "tagger");
-//            config.GetStringList($"event-adapter-bindings.\"{typeof(EventAdapters.Event2).TypeQualifiedName()}\"").Should()
-//                .BeEquivalentTo("combo", "tagger");
-
-//            config.GetString("event-adapters.mapper1").Should().Be(typeof(EventAdapters.EventMapper1).TypeQualifiedName());
-//            config.GetString("event-adapters.reader1").Should().Be(typeof(EventAdapters.ReadAdapter).TypeQualifiedName());
-//            config.GetString("event-adapters.combo").Should().Be(typeof(EventAdapters.ComboAdapter).TypeQualifiedName());
-//            config.GetString("event-adapters.tagger").Should().Be(typeof(EventAdapters.Tagger).TypeQualifiedName());
-
-//        }
-
-//        const string Json = @"
-//{
-//  ""Logging"": {
-//    ""LogLevel"": {
-//      ""Default"": ""Information"",
-//      ""Microsoft.AspNetCore"": ""Warning""
-//    }
-//  },
-//  ""Akka"": {
-//    ""JournalOptions"": {
-//      ""StoredAs"": ""JsonB"",
-//      ""UseBigIntIdentityForOrderingColumn"": true,
-
-//      ""ConnectionString"": ""Server=localhost,1533;Database=Akka;User Id=sa;"",
-//      ""ConnectionTimeout"": ""00:00:55"",
-//      ""SchemaName"": ""schema"",
-//      ""TableName"" : ""journal"",
-//      ""MetadataTableName"": ""meta"",
-//      ""SequentialAccess"": false,
-
-//      ""IsDefaultPlugin"": false,
-//      ""Identifier"": ""custom"",
-//      ""AutoInitialize"": true,
-//      ""Serializer"": ""hyperion""
-//    }
-//  }
-//}";
-
-//        [Fact(DisplayName = "MongoDbJournalOptions should be bindable to IConfiguration")]
-//        public void JournalOptionsIConfigurationBindingTest()
-//        {
-//            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(Json));
-//            var jsonConfig = new ConfigurationBuilder().AddJsonStream(stream).Build();
-
-//            var options = jsonConfig.GetSection("Akka:JournalOptions").Get<MongoDbJournalOptions>();
-//            options.IsDefaultPlugin.Should().BeFalse();
-//            options.Identifier.Should().Be("custom");
-//            options.AutoInitialize.Should().BeTrue();
-//            options.Serializer.Should().Be("hyperion");
-//            options.ConnectionString.Should().Be("Server=localhost,1533;Database=Akka;User Id=sa;");
-//            options.ConnectionTimeout.Should().Be(55.Seconds());
-//            options.SchemaName.Should().Be("schema");
-//            options.TableName.Should().Be("journal");
-//            options.MetadataTableName.Should().Be("meta");
-//            options.SequentialAccess.Should().BeFalse();
-
-//            options.StoredAs.Should().Be(StoredAsType.JsonB);
-//            options.UseBigIntIdentityForOrderingColumn.Should().BeTrue();
-//        }
-
-
-        private static void AssertJournalConfig(Config underTest, Config reference)
+        [Fact(DisplayName = "MongoDbJournalOptions should generate proper config")]
+        public void JournalOptionsTest()
         {
-            underTest.GetString("class").Should().Be(reference.GetString("class"));
-            underTest.GetString("connection-string").Should().Be(reference.GetString("connection-string"));
-            underTest.GetBoolean("use-write-transaction").Should().Be(reference.GetBoolean("use-write-transaction"));
-            underTest.GetBoolean("auto-initialize").Should().Be(reference.GetBoolean("auto-initialize"));
-            underTest.GetString("plugin-dispatcher").Should().Be(reference.GetString("plugin-dispatcher"));
-            underTest.GetString("collection").Should().Be(reference.GetString("collection"));
-            underTest.GetString("metadata-collection").Should().Be(reference.GetString("metadata-collection"));
-            underTest.GetBoolean("legacy-serialization").Should().Be(reference.GetBoolean("legacy-serialization"));
-            underTest.GetTimeSpan("call-timeout").Should().Be(reference.GetTimeSpan("call-timeout"));
+            var options = new MongoDbJournalOptions(true)
+            {
+                Identifier = "custom",
+                AutoInitialize = true,
+                ConnectionString = "testConnection",
+                Collection = "testCollection",
+                MetadataCollection = "metadataCollection",
+                UseWriteTransaction = true,
+                LegacySerialization = true,
+                CallTimeout = TimeSpan.FromHours(2)
+            };
+
+            var baseConfig = options.ToConfig();
+
+            baseConfig.GetString("akka.persistence.journal.plugin").Should().Be("akka.persistence.journal.custom");
+
+            var config = baseConfig.GetConfig("akka.persistence.journal.custom");
+            config.Should().NotBeNull();
+            config.GetString("connection-string").Should().Be(options.ConnectionString);
+            config.GetBoolean("auto-initialize").Should().Be(options.AutoInitialize);
+            config.GetString("collection").Should().Be(options.Collection);
+            config.GetString("metadata-collection").Should().Be(options.MetadataCollection);
+            config.GetBoolean("use-write-transaction").Should().Be(options.UseWriteTransaction);
+            config.GetBoolean("legacy-serialization").Should().Be(options.LegacySerialization);
+            config.GetTimeSpan("call-timeout").Should().Be(options.CallTimeout);
+
         }
+
+        //        const string Json = @"
+        //{
+        //  ""Logging"": {
+        //    ""LogLevel"": {
+        //      ""Default"": ""Information"",
+        //      ""Microsoft.AspNetCore"": ""Warning""
+        //    }
+        //  },
+        //  ""Akka"": {
+        //    ""JournalOptions"": {
+        //      ""StoredAs"": ""JsonB"",
+        //      ""UseBigIntIdentityForOrderingColumn"": true,
+
+        //      ""ConnectionString"": ""Server=localhost,1533;Database=Akka;User Id=sa;"",
+        //      ""ConnectionTimeout"": ""00:00:55"",
+        //      ""SchemaName"": ""schema"",
+        //      ""TableName"" : ""journal"",
+        //      ""MetadataTableName"": ""meta"",
+        //      ""SequentialAccess"": false,
+
+        //      ""IsDefaultPlugin"": false,
+        //      ""Identifier"": ""custom"",
+        //      ""AutoInitialize"": true,
+        //      ""Serializer"": ""hyperion""
+        //    }
+        //  }
+        //}";
+
+        //        [Fact(DisplayName = "MongoDbJournalOptions should be bindable to IConfiguration")]
+        //        public void JournalOptionsIConfigurationBindingTest()
+        //        {
+        //            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(Json));
+        //            var jsonConfig = new ConfigurationBuilder().AddJsonStream(stream).Build();
+
+        //            var options = jsonConfig.GetSection("Akka:JournalOptions").Get<MongoDbJournalOptions>();
+        //            options.IsDefaultPlugin.Should().BeFalse();
+        //            options.Identifier.Should().Be("custom");
+        //            options.AutoInitialize.Should().BeTrue();
+        //            options.Serializer.Should().Be("hyperion");
+        //            options.ConnectionString.Should().Be("Server=localhost,1533;Database=Akka;User Id=sa;");
+        //            options.ConnectionTimeout.Should().Be(55.Seconds());
+        //            options.SchemaName.Should().Be("schema");
+        //            options.TableName.Should().Be("journal");
+        //            options.MetadataTableName.Should().Be("meta");
+        //            options.SequentialAccess.Should().BeFalse();
+
+        //            options.StoredAs.Should().Be(StoredAsType.JsonB);
+        //            options.UseBigIntIdentityForOrderingColumn.Should().BeTrue();
+        //        }
+
+
     }
 }
