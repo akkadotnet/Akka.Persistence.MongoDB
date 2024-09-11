629b1025f139b0ab13a4149d45b4d7463bbaab03
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-09-12 03:37:01 +0700

    Add SnapshotStore SaveSnapshot spec for GridFS from TCK

---
+++ src/Akka.Persistence.MongoDb.Tests/GridFS/MongoDbGridFsSnapshotStoreSaveSnapshotSpec.cs
@@ -0,0 +1,38 @@
+﻿using Akka.Configuration;
+using Akka.Persistence.TCK.Snapshot;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.MongoDb.Tests.GridFS;
+
+[Collection("MongoDbSpec")]
+public class MongoDbGridFsSnapshotStoreSaveSnapshotSpec: SnapshotStoreSaveSnapshotSpec, IClassFixture<DatabaseFixture>
+{
+    public MongoDbGridFsSnapshotStoreSaveSnapshotSpec(DatabaseFixture databaseFixture, ITestOutputHelper output) 
+        : base(CreateSpecConfig(databaseFixture), nameof(MongoDbGridFsSnapshotStoreSaveSnapshotSpec), output)
+    {
+    }
+
+    private static Config CreateSpecConfig(DatabaseFixture databaseFixture)
+    {
+        var specString = $$"""
+                           akka.test.single-expect-default = 3s
+                           akka.persistence {
+                              publish-plugin-commands = on
+                              snapshot-store {
+                                  plugin = "akka.persistence.snapshot-store.mongodb"
+                                  mongodb {
+                                      class = "Akka.Persistence.MongoDb.Snapshot.MongoDbGridFsSnapshotStore, Akka.Persistence.MongoDb"
+                                      connection-string = "{{databaseFixture.ConnectionString}}"
+                                      use-write-transaction = off
+                                      auto-initialize = on
+                                      collection = "SnapshotStore"
+                                  }
+                              }
+                           }
+                           """;
+
+        return ConfigurationFactory.ParseString(specString);
+    }
+
+}
